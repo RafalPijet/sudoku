7f22a341f480ed8cb2a37f921767845720a58d9d
Rafal Pijet <stronglopez@wp.pl>
2019-08-10 17:37:15 +0200

    Fix SelectLevel component

--- src/components/SelectLevel.js
+++ src/components/SelectLevel.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SelectLevel = props => (
     <div className="cell col-12">
-        <input onClick={(event) => event.stopPropagation()} type="radio" name={props.level} value={props.level}
+        <input onClick={(event) => event.stopPropagation()} type="radio" name="level" value={props.level}
                onChange={(event) => props.setLevel(event.target.value)} id={props.level}/>
         <label htmlFor={props.level}>{props.level}</label>
     </div>
